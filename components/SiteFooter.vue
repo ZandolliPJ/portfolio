8beab3549921699cfd96d5741f1a13af73aa6ae9
Pascal1350 <152286354+Pascal1350@users.noreply.github.com>
2024-01-04 13:04:01 +0100

    First Commit

---
+++ components/SiteFooter.vue
@@ -0,0 +1,20 @@
+<script setup>
+const links = [
+    { name: 'GitHub', href: 'https://github.com/GregoryValsaint', icon: 'entypo-social:github-with-circle'},
+    { name: 'LinkedIn', href: 'https://www.linkedin.com/in/gr%C3%A9gory-valsaint-5913ba173', icon: 'entypo-social:linkedin-with-circle' },
+    //{ name: 'Instagram', href: 'https://instagram.com', icon: 'entypo-social:instagram' }
+]
+</script>
+ 
+<template>
+  <footer class="bg-gray-800">
+    <section class="mx-auto max-w-7xl px-2 sm:px-4 lg:py-6 text-gray-300 flex flex-row">
+      <p class="flex-1">&copy;Pascal Valsaint 2023. All Rights Reserved</p>
+      <div class="flex flex-row justify-center gap-4 mb-4 mt-2">
+        <a v-for="link in links" :key="link.name" :href="link.href">
+          <Icon :name="link.icon" size="1.5rem" class="text-gray-300" />
+        </a>
+      </div>
+    </section>
+  </footer>
+</template>
