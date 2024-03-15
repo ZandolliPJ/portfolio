244d621ad7a578cc72cba4b39e50343386c569d2
Pascal1350 <152286354+Pascal1350@users.noreply.github.com>
2024-03-15 17:10:44 +0100

    CliquerSurGithub

--- pages/projects.vue
+++ pages/projects.vue
@@ -3,7 +3,7 @@
     <div class="container has-text-centered">
       <h1 class="title">My Projects</h1>
       <p>
-        Some of my projects can be found sur <a href="https://github.com/ZandolliPJ" target="_blank">GitHub</a>.
+        Some of my projects can be found cliquer sur <a href="https://github.com/ZandolliPJ" target="_blank">GitHub</a>.
       </p>
     </div>
   </section>
